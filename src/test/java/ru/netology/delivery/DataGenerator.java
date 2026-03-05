239066277b55ba83d008addc79db293c811b6f8b
nikita <nikitka.shapovalov.99.99@mail.ru>
2026-03-05 15:57:48 +0300

    Complete Patterns homework: Delivery Card Replan

--- src/test/java/ru/netology/delivery/DataGenerator.java
+++ src/test/java/ru/netology/delivery/DataGenerator.java
@@ -9,21 +9,30 @@ import java.util.Locale;
 import java.util.Random;
 
 public class DataGenerator {
-    private DataGenerator() {
+    private DataGenerator () {
     }
 
-    public static String generateDate(int addDays) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     public static String generateCity() {
-        var cities = new String[]{"Москва", "Тула", "Рязань", "Ярославль", "Тверь", "Владимир", "Калуга", "Тамбов", "Самара", "Саратов"};
+        var cities = new String[]{"Майкоп", "Горно-Алтайск", "Уфа", "Улан-Удэ", "Махачкала", "Магас", "Нальчик",
+                "Элиста", "Черкесск", "Петрозаводск", "Сыктывкар", "Симферополь", "Йошкар-Ола", "Саранск", "Якутск",
+                "Владикавказ", "Казань", "Кызыл", "Ижевск", "Абакан", "Грозный", "Чебоксары", "Барнаул", "Чита",
+                "Петропавловск-Камчатский", "Краснодар", "Красноярск", "Пермь", "Владивосток", "Ставрополь",
+                "Хабаровск", "Благовещенск", "Архангельск", "Астрахань", "Белгород", "Брянск", "Владимир",
+                "Волгоград", "Вологда", "Воронеж", "Иваново", "Иркутск", "Калининград", "Калуга", "Кемерово",
+                "Киров", "Кострома", "Курган", "Курск", "Липецк", "Магадан", "Мурманск", "Нижний Новгород",
+                "Великий Новгород", "Новосибирск", "Омск", "Оренбург", "Орёл", "Пенза", "Псков", "Ростов-на-Дону",
+                "Рязань", "Самара", "Саратов", "Южно-Сахалинск", "Екатеринбург", "Смоленск", "Тамбов", "Тверь",
+                "Томск", "Тула", "Тюмень", "Ульяновск", "Челябинск", "Ярославль", "Москва", "Санкт-Петербург",
+                "Севастополь", "Биробиджан", "Нарьян-Мар", "Ханты-Мансийск", "Анадырь", "Салехард"};
         return cities[new Random().nextInt(cities.length)];
     }
 
     public static String generateName(String locale) {
         var faker = new Faker(new Locale(locale));
-        // Генерирует имя и фамилию на русском языке
         return faker.name().lastName() + " " + faker.name().firstName();
     }
 
@@ -33,7 +42,7 @@ public class DataGenerator {
     }
 
     public static class Registration {
-        private Registration() {
+        private Registration (){
         }
 
         public static UserInfo generateUser(String locale) {
